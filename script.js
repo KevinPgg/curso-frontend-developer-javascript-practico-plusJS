d4d01ffd063cfa4b27d7695466f0631fa48774a6
KevinPgg <kkevinpalaciosgg@outlook.com>
2023-11-12 16:28:35 -0500

    Funcionalidad boton de inicio, funcionalidad boton carrito de compras, responsive

--- script.js
+++ script.js
@@ -3,20 +3,39 @@ const navREmail = document.querySelector(".navbar-email");
 const carritoCompraR = document.querySelector(".navbar-shopping-cart");
 const menuCategorias= document.querySelector(".menu");
 const menuMovil = document.querySelector(".mobile-menu");
+const detalleOrden= document.querySelector(".product-detail")
 
 navREmail.addEventListener('mousedown',preventDefault)
 
 navREmail.addEventListener("click",toggleDesktopMenu);
 
+
 menuCategorias.addEventListener("click",function(){
     menuMovil.classList.toggle('inactive');
 })
 
+window.addEventListener('resize',function(){
+    if(window.innerWidth > 640){
+        menuMovil.classList.add('inactive');
+    }
+    
+
+});
+
+carritoCompraR.addEventListener('click',toggleShoppingMenu);
 
 
 
 function toggleDesktopMenu(){
     menuRDesk.classList.toggle('inactive');
+}
+function toggleShoppingMenu(){
+    if(window.innerHeight<640){
+        detalleOrden.classList.toggle('inactive');
+    
+    }else{
+        
+    }
 }
 
 function preventDefault(evt){
