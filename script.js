01798ea58144aebb7a6979a380bc491bb301abcf
KevinPgg <kkevinpalaciosgg@outlook.com>
2023-11-13 11:52:38 -0500

    mayor funcionalidad mobile y desktop, estetica menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuRDesk = document.querySelector(".desktop-menu");
 const navREmail = document.querySelector(".navbar-email");
+const navLEmail = document.querySelector(".nav-email");
 const carritoCompraR = document.querySelector(".navbar-shopping-cart");
 const menuCategorias= document.querySelector(".menu");
 const menuMovil = document.querySelector(".mobile-menu");
@@ -7,36 +8,37 @@ const detalleOrden= document.querySelector(".product-detail")
 
 navREmail.addEventListener('mousedown',preventDefault)
 
-navREmail.addEventListener("click",toggleDesktopMenu);
+navREmail.addEventListener("click",function(){
+    menuRDesk.classList.toggle('inactive');
+    detalleOrden.classList.add('inactive')
+});
 
+
+navLEmail.addEventListener('mousedown',preventDefault)
+
+navLEmail.addEventListener("click",function(){
+    menuRDesk.classList.toggle('inactive');
+});
 
 menuCategorias.addEventListener("click",function(){
     menuMovil.classList.toggle('inactive');
+    menuRDesk.classList.add('inactive');
+    
 })
 
 window.addEventListener('resize',function(){
     if(window.innerWidth > 640){
         menuMovil.classList.add('inactive');
+        menuRDesk.classList.add('inactive');
     }
     
-
 });
 
-carritoCompraR.addEventListener('click',toggleShoppingMenu);
+carritoCompraR.addEventListener('click',function(){
+    detalleOrden.classList.toggle('inactive');
+    menuRDesk.classList.add('inactive');
+});
 
-
-
-function toggleDesktopMenu(){
-    menuRDesk.classList.toggle('inactive');
-}
-function toggleShoppingMenu(){
-    if(window.innerHeight<640){
-        detalleOrden.classList.toggle('inactive');
-    
-    }else{
-        
-    }
-}
 
 function preventDefault(evt){
     evt.preventDefault()
